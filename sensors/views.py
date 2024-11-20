fc04c3caf04c7b5a85fbb5f7cb560b01c1a492de
ffont <frederic.font@upf.edu>
2024-11-20 12:20:57 +0100

    Improve graph options

--- sensors/views.py
+++ sensors/views.py
@@ -104,37 +104,179 @@ def list_sensor_ids(request):
     return Response(sensor_ids, status=status.HTTP_200_OK)
 
 
-def sensor_data_view(request, sensor_id):
-    # View to show real-time sensor data
-    return render(request, 'sensors/sensor_data.html', { 
-        'sensor_id': sensor_id,
-        'api_endpoint_url': os.getenv('API_BASE_URL') +  f'/sensor-data/'
-    })
+def _process_sensor_data_form(request):
+    start_time = request.GET.get('start_time', None)
+    end_time = request.GET.get('end_time', None)
+    if start_time == '':
+        start_time = None
+    if end_time == '':
+        end_time = None
+    if start_time is None or end_time is None:  # If only one is provided, set both to None
+        start_time = None
+        end_time = None
+    time_range = request.GET.get('time_range', None)
+    real_time = request.GET.get('real_time', False)
+    time_range_options = [
+        (1, 'Last Minute'),
+        (10, 'Last 10 Minutes'),
+        (30, 'Last 30 Minutes'),
+        (60, 'Last Hour'),
+        (720, 'Last 12 Hours'),
+        (1440, 'Last 24 Hours')
+    ]
+    default_time_range = 10
 
+    if start_time is None and end_time is None and time_range is None:
+        time_range = default_time_range
+    elif start_time is not None and end_time is not None:
+        time_range = int(time_range)
+        try:
+            start_time = datetime.datetime.strptime(start_time, '%Y-%m-%dT%H:%M')
+            end_time = datetime.datetime.strptime(end_time, '%Y-%m-%dT%H:%M')
+        except ValueError:
+            start_time = None
+            end_time = None
+    elif start_time is None and end_time is None and time_range is not None:
+        time_range = int(time_range)
+    
+    available_sensor_ids = sorted(list(set(SensorData.objects.filter(sensor_timestamp__range=(start_time, end_time)).values_list('sensor_id', flat=True))))
+    sensor_ids = request.GET.getlist('sensors', [])
+    if sensor_ids == []:
+        sensor_ids = available_sensor_ids
+    sensor_ids = [s for s in sensor_ids if s in available_sensor_ids]  # Make sure we only include valid sensor IDs
 
-def multiple_sensor_data_view(request):
-    # View to show real-time sensor data for multiple sensors
-    start_time = request.GET.get('start_time')
-    end_time = request.GET.get('end_time')
-
-    if not start_time or not end_time:
-        start_time = (datetime.datetime.now() - datetime.timedelta(hours=1)).strftime('%Y-%m-%dT%H:%M')
-        end_time = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M')
-
-    try:
-        start_time = datetime.datetime.strptime(start_time, '%Y-%m-%dT%H:%M')
-        end_time = datetime.datetime.strptime(end_time, '%Y-%m-%dT%H:%M')
-    except ValueError:
-        raise Exception("Invalid time format. Please use 'YYYY-MM-DDTHH:MM'.")
-        
-    sensor_ids = sorted(list(set(SensorData.objects.filter(sensor_timestamp__range=(start_time, end_time)).values_list('sensor_id', flat=True))))
-
-    return render(request, 'sensors/multiple_sensor_data.html', {
+    return {
+        'available_sensor_ids': available_sensor_ids,
         'sensor_ids': sensor_ids,
         'start_time': start_time,
         'end_time': end_time,
-        'api_endpoint_url': os.getenv('API_BASE_URL') + f'/sensor-data/'
+        'time_range': time_range,
+        'real_time': real_time,
+        'time_range_options': time_range_options,
+        'time_range_mode': start_time is None and end_time is None,
+        'real_time_mode': real_time and (start_time is None and end_time is None),
+    }
+
+
+def sensor_data_view(request, sensor_id):
+    charts = [
+        {
+            'title': 'SPL',
+            'data': [
+                {'label': 'Leq', 'data_path': 'leq', 'color': 'blue'},
+                {'label': 'LAeq', 'data_path': 'LAeq', 'color': 'red'}
+            ],
+            'y_min': 40,
+            'y_max': 90,
+            'y_unit': 'dB'
+        },
+        {
+            'title': 'Perceptual Attributes',
+            'data': [
+                {'label': 'Pleasantness', 'data_path': 'pleasantness_intg', 'color': 'blue'},
+                {'label': 'Pleasantness inst', 'data_path': 'pleasantness_inst', 'color': 'darkBlue', 'dash': True},
+                {'label': 'Eventfulness', 'data_path': 'eventfulness_intg', 'color': 'green'},
+                {'label': 'Eventfulness inst', 'data_path': 'eventfulness_inst', 'color': 'darkGreen', 'dash': True}
+            ],
+            'y_min': -0.5,
+            'y_max': 0.5,
+            'y_unit': ''
+        },
+        {
+            'title': 'Detected Sources',
+            'data': [
+                {'label': 'Birds', 'data_path': 'sources.birds', 'color': 'red'},
+                {'label': 'Construction', 'data_path': 'sources.construction', 'color': 'orange'},
+                {'label': 'Dogs', 'data_path': 'sources.dogs', 'color': 'purple'},
+                {'label': 'Human', 'data_path': 'sources.human', 'color': 'brown'},
+                {'label': 'Music', 'data_path': 'sources.music', 'color': 'pink'},
+                {'label': 'Nature', 'data_path': 'sources.nature', 'color': 'gray'},
+                {'label': 'Siren', 'data_path': 'sources.siren', 'color': 'black'},
+                {'label': 'Vehicles', 'data_path': 'sources.vehicles', 'color': 'cyan'},
+            ],
+            'y_min': 0.0,
+            'y_max': 1.0,
+            'y_unit': ''
+        }
+    ]
+    tvars = _process_sensor_data_form(request)
+    tvars.update({
+        'sensor_ids': [sensor_id],
+        'single_sensor': True,
+        'api_endpoint_url': os.getenv('API_BASE_URL') + f'/sensor-data/',
+        'charts': charts
     })
+    return render(request, 'sensors/sensor_data.html', tvars)
+
+
+def multiple_sensor_data_view(request):
+    charts = [
+        {
+            'title': 'SPL - Leq',
+            'data': [
+                {'label': 'Leq', 'data_path': 'leq', 'color': 'blue'}
+            ],
+            'y_min': 40,
+            'y_max': 90,
+            'y_unit': 'dB'
+        },
+        {
+            'title': 'SPL - LAeq',
+            'data': [
+                {'label': 'LAeq', 'data_path': 'LAeq', 'color': 'red'}
+            ],
+            'y_min': 40,
+            'y_max': 90,
+            'y_unit': 'dB'
+        },
+        {
+            'title': 'Perceptual Attributes - Pleasantness',
+            'data': [
+                {'label': 'Pleasantness', 'data_path': 'pleasantness_intg', 'color': 'blue'},
+                {'label': 'Pleasantness inst', 'data_path': 'pleasantness_inst', 'color': 'darkBlue', 'dash': True},
+            ],
+            'y_min': -0.5,
+            'y_max': 0.5,
+            'y_unit': ''
+        },
+        {
+            'title': 'Perceptual Attributes - Eventfulness',
+            'data': [
+                {'label': 'Eventfulness', 'data_path': 'eventfulness_intg', 'color': 'green'},
+                {'label': 'Eventfulness inst', 'data_path': 'eventfulness_inst', 'color': 'darkGreen', 'dash': True}
+            ],
+            'y_min': -0.5,
+            'y_max': 0.5,
+            'y_unit': ''
+        }
+    ]
+    sources_data = [
+        {'label': 'Birds', 'data_path': 'sources.birds', 'color': 'red'},
+        {'label': 'Construction', 'data_path': 'sources.construction', 'color': 'orange'},
+        {'label': 'Dogs', 'data_path': 'sources.dogs', 'color': 'purple'},
+        {'label': 'Human', 'data_path': 'sources.human', 'color': 'brown'},
+        {'label': 'Music', 'data_path': 'sources.music', 'color': 'pink'},
+        {'label': 'Nature', 'data_path': 'sources.nature', 'color': 'gray'},
+        {'label': 'Siren', 'data_path': 'sources.siren', 'color': 'black'},
+        {'label': 'Vehicles', 'data_path': 'sources.vehicles', 'color': 'cyan'},
+    ]
+    for source in sources_data:
+        charts.append({
+            'title': f'Detected Sources - {source["label"]}',
+            'data': [source],
+            'y_min': 0.0,
+            'y_max': 1.0,
+            'y_unit': ''
+        })
+
+
+    tvars = _process_sensor_data_form(request)
+    tvars.update({
+        'single_sensor': False,
+        'api_endpoint_url': os.getenv('API_BASE_URL') + f'/sensor-data/',
+        'charts': charts
+    })
+    return render(request, 'sensors/sensor_data.html', tvars)
 
 def frontpage(request):
     return render(request, 'sensors/frontpage.html', {
